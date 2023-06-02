1642dabf69b3a7d1f53794198017c755bc138d95
Equipe BI <131811006+GueltBI@users.noreply.github.com>
2023-06-02 16:24:57 -0300

    Update urls.py

--- mainn/urls.py
+++ mainn/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('/base',views.home, name = 'home'),
     path('/comissão',views.home1, name = 'home1'),
     path('/captação',views.home2, name = 'home2'),
-    path('/ROA',views.home3, name = 'home3')
+    path('/ROA',views.home3, name = 'home3'),
+    path('/alocacao',views.home4, name = 'home4'),
 
 ]
