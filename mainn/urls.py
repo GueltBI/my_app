118f29750d096f4d15ff237c4ee7c0816e296e6e
Equipe BI <131811006+GueltBI@users.noreply.github.com>
2023-12-19 15:09:18 -0300

    Update urls.py

--- mainn/urls.py
+++ mainn/urls.py
@@ -4,12 +4,6 @@ from . import views
 
 urlpatterns = [
 
-    path('/base',views.home, name = 'home'),
-    path('/comissão',views.home1, name = 'home1'),
-    path('/captação',views.home2, name = 'home2'),
-    path('/ROA',views.home3, name = 'home3'),
-    path('/alocacao',views.home4, name = 'home4'),
-    path('/estruturadas',views.home5, name = 'home5'),
-    path('/AAI',views.home6, name = 'home6'),
-    path('/RD',views.home7, name = 'home7')
+    path('/base',views.home, name = 'home')
+
 ]
